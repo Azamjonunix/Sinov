67338e03f28a590294bad8e33a050488f9054e02
Azamjonunix <101107529+Azamjonunix@users.noreply.github.com>
2022-03-15 16:00:02 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/sinov/MainActivity.kt
+++ app/src/main/java/com/example/sinov/MainActivity.kt
@@ -24,3 +24,31 @@ class MainActivity : AppCompatActivity(){
     }
 
 }
+
+// boshqasi
+package com.example.sinov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+// bitta matndi ikki hil joyini kesib olamiz
+class MainActivity : AppCompatActivity(){
+    var text: String ="в магазине <осталось 25> xvxvkldfjgs <dkgjd> df dgv "
+    var tvText: TextView? = null // birinchi textni idsi
+    var textikki: TextView? = null // ikkinchi textni idsi
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvText = findViewById(R.id.tvText) // birinchi textni id ordali topyapmiz
+        textikki = findViewById(R.id.textikki) // ikkinchi textni id ordali topyapmiz
+        val subText:String = text.substringAfter('<')
+        val subText2:String = subText.substringBefore('>')
+        val subText3:String = text.substringAfter('>')
+        val subText4:String = subText3.substringAfter('<')
+        val subText5:String = subText4.substringBefore('>')
+        textikki?.setText(subText5)
+        tvText?.setText(subText2)
+    }
+
+}
